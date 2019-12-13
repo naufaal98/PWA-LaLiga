1002cb36e508869598a8c95117469a583bf1f7a0
Muhammad Naufal <muhammad_naufal@hotmail.com>
2019-12-13 18:31:06 +0700

    workbox

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,56 @@
-const CACHE_NAME = "football-v3";
-var urlsToCache = [
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "/",
-  "/manifest.json",
-  "/nav.html",
-  "/index.html",
-  "/teamDetail.html",
-  "/push.js",
-  "/pages/home.html",
-  "/pages/favorite.html",
-  "/css/materialize.min.css",
-  "/css/custom.css",
-  "/js/materialize.min.js",
-  "/js/nav.js",
-  "/js/api.js",
-  "/js/idb.js",
-  "/js/db.js",
-  "/js/script.js",
-  "/images/icons/icon-72x72.png",
-  "/images/icons/icon-96x96.png",
-  "/images/icons/icon-128x128.png",
-  "/images/icons/icon-144x144.png",
-  "/images/icons/icon-152x152.png",
-  "/images/icons/icon-192x192.png",
-  "/images/icons/icon-384x384.png",
-  "/images/icons/icon-512x512.png",
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
+
+workbox.precaching.precacheAndRoute([
+  { url: '/', revision: '1' },
+  { url: '/manifest.json', revision: '1' },
+  { url: '/nav.html', revision: '1' },
+  { url: '/index.html', revision: '1' },
+  { url: '/teamDetail.html', revision: '1' },
+  { url: '/push.js', revision: '1' },
+  { url: '/pages/home.html', revision: '1' },
+  { url: '/pages/favorite.html', revision: '1' },
+  { url: '/css/materialize.min.css', revision: '1' },
+  { url: '/css/custom.css', revision: '1' },
+  { url: '/js/materialize.min.js', revision: '1' },
+  { url: '/js/api.js', revision: '1' },
+  { url: '/js/nav.js', revision: '1' },
+  { url: '/js/idb.js', revision: '1' },
+  { url: '/js/db.js', revision: '1' },
+  { url: '/js/script.js', revision: '1' },
+  { url: '/images/icons/icon-72x72.png', revision: '1' },
+  { url: '/images/icons/icon-96x96.png', revision: '1' },
+  { url: '/images/icons/icon-128x128.png', revision: '1' },
+  { url: '/images/icons/icon-144x144.png', revision: '1' },
+  { url: '/images/icons/icon-152x152.png', revision: '1' },
+  { url: '/images/icons/icon-192x192.png', revision: '1' },
+  { url: '/images/icons/icon-384x384.png', revision: '1' },
+  { url: '/images/icons/icon-512x512.png', revision: '1' },
+],
+{
+  ignoreUrlParametersMatching: [/.*/]
+});
  
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/'),
+  workbox.strategies.staleWhileRevalidate()
+);
 
-self.addEventListener("fetch", function(event) {
-  const base_url_football = "https://api.football-data.org/v2/"
-  if (event.request.url.indexOf(base_url_football) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-          return response || fetch (event.request);
-      })
-    )
-  }
-});
+workbox.routing.registerRoute(
+  new RegExp('https://api.football-data.org/v2/'),
+  workbox.strategies.staleWhileRevalidate()
+)
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
 
 self.addEventListener('push', function(event) {
   var body;
